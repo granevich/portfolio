222430b41d9c14ea087f717e9f0ba87ae79076b7
Александр Граневич < >
2017-02-05 23:34:04 +0200

    making particles

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by granevich on 06.05.2016.
+ */
+document.onload = function () {
+    particlesJS.load('particles-js', 'data/particlesjs-config(1).json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+};
