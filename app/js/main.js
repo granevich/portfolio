c78cdce5f3898b6c3b3658eabedd6ce84bd3f93b
Granevich <granevich@gmail.com>
2017-02-06 17:00:02 +0200

    hello page done add preloader and blur effect  06.02.2017 16:57

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,12 @@
  * Created by granevich on 06.05.2016.
  */
 
-
+$('.preloader').show();
+$('.card').css('filter','blur(5px)');
 $(document).ready(function () {
-    $('.preloader').show();
-    $('.card').css('filter','blur(5px)');
 
-    setTimeout(function () {
-        $('.preloader').hide();
-        $('.card').css('filter','blur(0)');
-    }, 1500)
+
+            $('.preloader').hide();
+            $('.card').css('filter','blur(0)');
 
 });
