2afeda6d8a585128d7c62038ce8a76c1e3365b27
Granevich <granevich@gmail.com>
2017-02-15 17:06:06 +0200

    recode skills page

--- app/js/cv.js
+++ app/js/cv.js
@@ -43,16 +43,61 @@ $(document).ready(function () {
 
     });
 //chart
-    $(function() {
-        $('.chart').easyPieChart({
-            barColor:'#28807A',
-            trackColor:'white',
-            scaleColor:'white',
-            loneCap:'butt',
-            lineWidth:10,
-            animate:true
-        });
+
+    Highcharts.chart('container', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Main web skills'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Skills',
+            colorByPoint: true,
+            data: [{
+                name: 'HTML5',
+                y: 25
+            }, {
+                name: 'Css3',
+                y: 25,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Javascript',
+                y: 10.38
+            }, {
+                name: 'PHP',
+                y: 4.77
+            }, {
+                name: 'Wordpress',
+                y: 0.91
+            }, {
+                name: 'MySQL',
+                y: 0.2
+            }]
+        }]
     });
+   var fill =  document.querySelector('.highcharts-background');
+   fill.setAttribute('fill', 'transparent');
 //chart
    var img =  document.querySelectorAll('img');
    var contentEdu = document.querySelector('.content_edu');
